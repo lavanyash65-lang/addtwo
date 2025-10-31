b8b333088d8d9b55961420e8c080012ac8afe542
lavanyash65-lang <lavanyash65@gmail.com>
2025-10-31 09:50:03 +0530

    Fix input function calls in add.py

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
+a = float("Enter first number: ")
+b = float("Enter second number: ")
 add = a + b
 print(f"The sum of {a} and {b} is {add}")
